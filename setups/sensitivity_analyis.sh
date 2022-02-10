390ad97d1d20681c095f4d9783654ba6ba71f598
Karsten Bolding <karsten@bolding-bruggeman.com>
2022-02-10 07:18:46 +0100

    updated sensitivity_analysis.sh

--- setups/sensitivity_analyis.sh
+++ setups/sensitivity_analyis.sh
@@ -9,18 +9,21 @@ N=20
 method=saltelli
 method=random
 
+info=${method}_${N}.pkl
+
 # sampling
-parsac sensitivity sample $config $method.pkl $method $N 
+parsac sensitivity sample ${config} ${info} ${method} $N 
 
 # running
-parsac sensitivity run $method.pkl
+parsac sensitivity run ${info}
 
 # analyzing
 if [ "$method" == "random" ]; then
-   parsac sensitivity analyze $method.pkl mvr >& analysis_${method}_${N}_mvr.txt
+   parsac sensitivity analyze ${info} mvr >& analysis_${method}_${N}_mvr.txt
 fi
 
 # analyzing
 if [ "$method" == "saltelli" ]; then
-   parsac sensitivity analyze $method.pkl sobol >& analysis_${method}_${N}_sobol.txt
+   parsac sensitivity analyze ${info} sobol >& analysis_${method}_${N}_sobol.txt
 fi
+
